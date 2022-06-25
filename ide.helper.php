4c9a4fde331d115e45cba0c1a158705c178abba9
Magomedcoder <info@magomedcoder.ru>
2022-06-25 20:28:45 +0300

    Added ide.helper

---
+++ ide.helper.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Psr\Http\Message {
+
+    use PCore\HttpMessage\Response;
+    use PCore\HttpMessage\ServerRequest;
+
+    /**
+     * @mixin ServerRequest
+     */
+    interface ServerRequestInterface
+    {
+    }
+
+    /**
+     * @mixin Response
+     */
+    interface ResponseInterface
+    {
+
+    }
+
+}
+
+namespace Psr\Container {
+
+    use PCore\Di\Container;
+
+    /**
+     * @mixin Container
+     */
+    interface ContainerInterface
+    {
+    }
+
+}
+
+namespace Psr\SimpleCache {
+
+    use PCore\Cache\Cache;
+
+    /**
+     * @mixin Cache
+     */
+    interface CacheInterface
+    {
+    }
+
+}
